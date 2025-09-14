795ab135cfae223d8d0acc039565da307bbe0ad6
seoyeong5864 <wendy2281@naver.com>
2025-09-14 18:36:28 +0900

    [Silver III] Title: N과 M (1), Time: 300 ms, Memory: 24796 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,56 +2,56 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	static int N; // 1부터 N까지의 자연수
 	static int M; // 선택할 개수
 	static int[] num; // 자연수 N개 저장 배열
 	static boolean[] sel; // 선택했는지 안했는지 저장
 	static int[] pick; // 선택한 숫자 저장
-	
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		// 입력
 		N = sc.nextInt();
 		M = sc.nextInt();
-		
+
 		num = new int[N];
-		for(int i = 0; i < N; i++) {
-			num[i] = i+1;
+		for (int i = 0; i < N; i++) {
+			num[i] = i + 1;
 		}
-		
+
 		pick = new int[M];
 		sel = new boolean[N];
-		
+
 		// 로직
-		dfs(0);
-		
-		
+		permutation(0);
+		System.out.println(sb);
+
 	}
 
-	private static void dfs(int idx) {
-		
+	private static void permutation(int sidx) {
+
 		// 종료조건
-		if(idx == M) {
-			for(int i = 0; i < M; i++) {
-				System.out.print(pick[i]+" ");
+		if (sidx == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(pick[i]).append(' ');
 			}
-			System.out.println();
+			sb.append('\n');
 			return;
 		}
-		
+
 		// 아직 사용 안한 수를 선택
-		for(int i = 0; i < N; i++) {
-			if(sel[i]) continue; // 선택했으면 패스
+		for (int i = 0; i < N; i++) {
+			if (sel[i])
+				continue; // 선택했으면 패스
 			sel[i] = true; // 선택처리
-			pick[idx] = num[i];
-			dfs(idx+1);
+			pick[sidx] = num[i];
+			permutation(sidx + 1);
 			sel[i] = false; // 되돌리기
 		}
-		
+
 	}
-	
-	
-	
+
 }
