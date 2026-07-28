018549d7b10ab39cd27a2b8e3896924d046d9829
seoyeong5864 <wendy2281@naver.com>
2026-07-28 09:32:27 +0900

    [level 1] Title: 추억 점수, Time: 10.99 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -3,17 +3,13 @@ class Solution {
         int[] answer = new int[photo.length];
         
         for(int i = 0; i < photo.length; i++){
-            int sum = 0;
             for(int j = 0; j < photo[i].length; j++){
-                
                 for(int k = 0; k < name.length; k++){
-                    
                     if(photo[i][j].equals(name[k])){
-                        sum += yearning[k];
+                        answer[i] += yearning[k];
                     }
                 }
             }
-            answer[i] = sum;
         }
         return answer;
     }
